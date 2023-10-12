8945c5688146aeeacf999859209eab90265aa843
Christian Bongard <christian.bongard90@gmail.com>
2023-10-12 09:29:23 +0400

    initialised the git

---
+++ javascript/application.js
@@ -0,0 +1,7 @@
+console.log("Hello from JavaScript!");
+
+const fullName = 'Chris';
+
+const message = `Hello, ${fullName}`;
+
+console.log(message); // only used in development or terminal only applications never for testing
